ef6032bdc8d48c784fd2bb33887515f4dd26c49c
joeblobstudent <r037928a@student.staffs.ac.uk>
2018-09-23 21:58:16 +0100

    tuning and comments

--- PokemonBattle.py
+++ PokemonBattle.py
@@ -1,31 +1,55 @@
-#Sequence! Code reads from left to right and top to bottom like English.
-#The compiler reads your code a bit like a person would read a book, unless
-#we are using Selection! or Iteration!
+# Sequence! Text based code reads from left to right and top to bottom like
+# English. The compiler reads your code a bit like a person would read a book,
+# unless we are using Selection! or Iteration!
 
 
-#We use import statements to access code from other code modules. This one
-#will give us random numbers.
+# We use import statements to access code from other code modules. This one
+# will give us random numbers.
 
 import random
 
-#Our first variables. Variables are the building blocks of our program. We are
-#resrving blocks in the computers memory to store the current health of our
-#Pokemon, so that we can keep score. We are also assigning each space with a
-#starting integer value. 
+# Our first variables. Variables are the building blocks of our program. Here
+# we are reserving blocks in the memory to store the current health of our
+# Pokemon, so that we can keep score. We are also assigning each space with a
+# starting integer value. An integer is a whole number that can't have a decimal
+# point.
 
-pikachuHealthPoints = 100
-snorlaxHealthPoints = 100
+pikachuHealthPoints = 60
+snorlaxHealthPoints = 60
+
+# An output (or print) to console statement. Here we are passing the built in
+# Python print() function a string of characters to print to the console.
+# The string will be all the characters within the quotation marks "".  
 
 print("A wild snorlax has appeared!")
 
+# Our first Iteration! or loop. All the code indented underneath the statement
+# while True: will go back to the top when it is finished and repeat. It will
+# continue to do that forever until reaches a break condition.
+
 while True:
+    
+    # We print out the variables we just created that are storing our
+    # Pokemon current health points to the console.
+    
     print("snorlax's health:",snorlaxHealthPoints)
     print("pikachu's health:",pikachuHealthPoints)
 
+    
+    # We now need to create some more variables to store the values attached
+    # to our actions. In future, every time we ask for the variable pikachuAttack
+    # in this example, we are requesting it to be assigned a random integer
+    # (whole number) between 7 and 12.
+    
     pikachuAttack = random.randint(7,12)
     pikachuHeal = random.randint(5,15)
     snorlaxAttack = random.randint(7,12)
     snorlaxHeal = random.randint(5,15)
+
+    # Our first Selection! statements. Before we do anything else we need to
+    # check if either of our characters has reached 0 health points,
+    # which would mean the end of the game. We use the operator <= (less than
+    # or equal to) to compare our health point variables to zero
 
     if pikachuHealthPoints <= 0 or snorlaxHealthPoints <= 0:
         print("GAME OVER")
@@ -39,8 +63,8 @@ while True:
         elif snorlaxMove == 2:
             print("snorlax HEALS FOR",snorlaxHeal,"POINTS!")
             snorlaxHealthPoints = snorlaxHealthPoints + snorlaxHeal
-            if snorlaxHealthPoints > 100:
-                snorlaxHealthPoints = 100
+            if snorlaxHealthPoints > 60:
+                snorlaxHealthPoints = 60
 
         print("snorlax's health:",snorlaxHealthPoints)
         print("pikachu health:",pikachuHealthPoints)
@@ -59,8 +83,8 @@ while True:
             print("YOU ATTACK FOR",pikachuAttack,"POINTS!")
             snorlaxHealthPoints = snorlaxHealthPoints - pikachuAttack
         elif pikachuMove == 2:
-            print("YOU HEAL YOURSELF FOR",pikachuHeal,"POINTS!")
+            print("YOU HEAL FOR",pikachuHeal,"POINTS!")
             pikachuHealthPoints = pikachuHealthPoints + pikachuHeal
-            if pikachuHealthPoints > 100:
-                pikachuHealthPoints = 100
+            if pikachuHealthPoints > 60:
+                pikachuHealthPoints = 60
     
